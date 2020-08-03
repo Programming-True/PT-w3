a833cf10b9ffcc5bddcadda3904ea18e5ed4d6f3
gameme10 <66317738+gameme10@users.noreply.github.com>
2020-08-03 11:44:41 +0200

    aswada :P

---
+++ PT-admin/login.php
@@ -0,0 +1,33 @@
+<?php
+
+$dbhost = "bnazaqudbafe8yuyljcr-mysql.services.clever-cloud.com";
+$dbuser = "u7cq07dkrxfcbyrq";
+$dbpass = "lCtSCj3tuSdxbGBQWQuM";
+$dbname = "bnazaqudbafe8yuyljcr";
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$conn) 
+{
+	die("No hay conexión: ".mysqli_connect_error());
+}
+
+$nombre = $_POST["txtusuario"];
+$pass = $_POST["txtpassword"];
+
+$query = mysqli_query($conn,"SELECT * FROM login WHERE usuario = '".$nombre."' and password = '".$pass."'");
+$nr = mysqli_num_rows($query);
+
+if($nr == 1)
+{
+	//header("Location: pagina.html")
+	echo "Bienvenido:" .$nombre;
+}
+else if ($nr == 0) 
+{
+	header("Location: login.html");
+	//echo "No ingreso"; 
+}
+	
+
+
+?>
