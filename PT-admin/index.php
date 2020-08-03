22d01be4191a7338561a4399e6eb54564863196d
gameme10 <66317738+gameme10@users.noreply.github.com>
2020-08-03 11:19:41 +0200

    Create index.php

---
+++ PT-admin/index.php
@@ -0,0 +1,30 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"
+	<?php include "includes/scripts.php"; ?>
+	<title>PT-admin<title>
+</head>
+<body>
+	<?php include "includes/header.php"; ?>
+	<section>
+		<div class="divContainer">
+			<div>
+				<h1 class="titlePanelControl">Panel de control</h1>
+			</div>
+		
+		</div>
+	
+	
+	
+	
+	
+	
+	</section>
+	<?php include "includes/footer.php"; ?>
+</body>
+</html>
